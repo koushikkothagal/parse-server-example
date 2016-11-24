cbdfa862b6ab7410b9255b27891b21ae050f6a3c
Koushik Kothagal <kkothagal@financialengines.com>
2016-11-24 14:45:54 -0800

    Adding courses data

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,12 @@
+var courses = require('../data/courses.json');
+
+
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+});
+
+
+Parse.Cloud.define('courses', function(req, res) {
+  res.success(courses);
 });
